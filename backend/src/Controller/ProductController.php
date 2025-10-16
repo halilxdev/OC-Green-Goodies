fc93e4bc854da0e5655cb0219ba2a711b195e9d2
Halil <halilxdev@halilxdev-AirM4.local>
2025-10-16 16:48:42 +0200

    Création d'une première route Product : getAllProducts avec un retour en JSON

---
+++ backend/src/Controller/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProductController extends AbstractController
+{
+/**
+     * Cette méthode permet de récupérer tous les produits
+     *
+     */
+    #[Route('/api/products', name: 'getAllProducts', methods: ['GET'])]
+    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $productList = $productRepository->findAll();
+        $jsonProductList = $serializer->serialize($productList, 'json');
+        return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
+    }
+}
