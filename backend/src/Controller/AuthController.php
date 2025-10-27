d3cf9cc1e02714ac78a12bcf810431b772cdc7cf
Halil <halilxdev@halilxdev-AirM4.local>
2025-10-27 11:19:02 +0100

    Installation du package JWT

---
+++ backend/src/Controller/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class AuthController extends AbstractController
+{
+    /**
+     * Cette méthode permet de créer un nouvel utilisateur.
+     * Exemple de données :
+     * {
+     *      "email": "votre@adresse.mail",
+     *      "password": "m0t_dePasse",
+     *      "zip_code": 12345
+     * }
+     *
+     * @return JsonResponse
+     */
+    #[Route('/api/user', name: 'createUser', methods: ['POST'])]
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $user = $serializer->deserialize($request->getContent(), type: User::class, format: 'json');
+        $user->setRoles(['ROLE_USER']);
+
+        $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hashedPassword);
+
+        $em->persist($user);
+        $em->flush();
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getAuthors']);
+
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, [], true);
+    }
+
+
+//     #[Route('/api/user/{id}', name:"updateUser", methods:['PUT'])]
+//     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour mettre à jour l\'utilisateur')]
+//     public function updateUser(Request $request, SerializerInterface $serializer, User $currentUser, EntityManagerInterface $em): JsonResponse 
+//     {
+//         $updatedUser = $serializer->deserialize($request->getContent(), 
+//                 User::class, 
+//                 'json', 
+//                 [AbstractNormalizer::OBJECT_TO_POPULATE => $currentUser]);
+//         $em->persist($updatedUser);
+//         $em->flush();
+//         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+//     }
+
+//     #[Route('/api/user/{id}', name: 'deleteUser', methods: ['DELETE'])]
+//     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour supprimer un utilisateur')]
+//     public function deleteUser(User $user, EntityManagerInterface $em): JsonResponse 
+//     {
+//         $em->remove($user);
+//         $em->flush();
+//         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+//     }
+
+}
