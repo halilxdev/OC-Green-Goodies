8cad8ab43edb7e1f447479f1223febe5fa2b5d6d
Halil <halilxdev@halilxdev-AirM4.local>
2025-11-04 11:38:26 +0100

    Changement du dossier frontend/ -> app/

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -15,22 +15,23 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function home(Request $request, HttpClientInterface $httpClient): Response
     {
-        try {
-            $response = $httpClient->request(
-                'GET',
-                sprintf('http://backend:8000/api/products')
-            );
-            $arrayProducts = $response->toArray();
-            return $this->render('home/index.html.twig', [
-                'controller_name'   =>  'HomeController',
-                'products'          =>  $arrayProducts,
-            ]);
-        } catch (\Exception $e) {
-            return new JsonResponse([
-                'error' => 'Erreur lors de l\'appel à l\'API',
-                'message' => $e->getMessage()
-            ], 500);
-        }
+        // try {
+        //     $response = $httpClient->request(
+        //         'GET',
+        //         sprintf('http://backend:8000/api/products')
+        //     );
+        //     $arrayProducts = $response->toArray();
+        //     return $this->render('home/index.html.twig', [
+        //         'controller_name'   =>  'HomeController',
+        //         'products'          =>  $arrayProducts,
+        //     ]);
+        // } catch (\Exception $e) {
+        //     return new JsonResponse([
+        //         'error' => 'Erreur lors de l\'appel à l\'API',
+        //         'message' => $e->getMessage()
+        //     ], 500);
+        // }
+        echo "hello"; die();
     }
     #[Route('/terms', name: 'terms')]
     public function terms(): Response
